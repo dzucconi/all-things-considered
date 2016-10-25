5cf3a30fa4e0f7d4433fbcae24427b3825da5db9
dzucconi <mail@damonzucconi.com>
2016-10-25 20:57:40 +0200

    Fill with as many instances as the window width allows (comfortably)

--- app/javascripts/index.js
+++ app/javascripts/index.js
@@ -4,31 +4,38 @@ import times from './lib/times';
 import Queue from './models/queue';
 import Chat from './models/chat';
 
-const PARAMS = params({
-  amount: 1,
-  size: 30,
-  pause: 1000,
-  mute: true,
-});
-
-const DOM = {
-  container: document.getElementById('container'),
-};
-
-const them = new Queue({
-  fetch: () => fetch(`http://dictionary.red/verse?n=${PARAMS.size}`),
-});
-
-const me = new Queue({
-  fetch: () => fetch(`http://dictionary.pink/verse?n=${PARAMS.size}`),
-});
-
 export default () => {
+  const PARAMS = params({
+    amount: Math.floor(window.innerWidth / 320) || 1,
+    size: 30,
+    pause: 1000,
+    mute: true,
+  });
+
+  const DOM = {
+    container: document.getElementById('container'),
+  };
+
+  const them = new Queue({
+    fetch: () => fetch(`http://dictionary.red/verse?n=${PARAMS.size}`),
+  });
+
+  const me = new Queue({
+    fetch: () => fetch(`http://dictionary.pink/verse?n=${PARAMS.size}`),
+  });
+
   them.fill()
     .then(() => me.fill())
     .then(() => {
-      const chats = times(PARAMS.amount).map(() =>
-        new Chat({ me, them, pause: PARAMS.pause, mute: PARAMS.mute }));
+      const chats = times(PARAMS.amount)
+        .map(() =>
+          new Chat({
+            me,
+            them,
+            pause: PARAMS.pause,
+            mute: PARAMS.mute
+          })
+        );
 
       DOM.container.innerHTML = chats.map(chat => chat.render().el).join('');
 
